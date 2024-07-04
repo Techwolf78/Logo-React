80dd639c9fc4714a6b48742fc8ff128b794c27c2
Techwolf78 <mrajaypawar207@gmail.com>
2024-07-04 12:05:21 +0530

    updated logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,4 +1,3 @@
-// src/Logo.tsx
 import React from 'react';
 
 const Logo: React.FC = () => {
@@ -7,7 +6,7 @@ const Logo: React.FC = () => {
       <div className="relative w-64 h-64">
         <div className="absolute top-0 left-0 w-full h-full border-8 border-transparent border-t-[#9BCD31] border-r-[#FF4500] border-b-[#A3A3A3] border-l-[#4682B4] transform rotate-45"></div>
         <div className="absolute inset-0 flex flex-col justify-center items-center text-white text-center">
-          <h1 className="text-2xl font-bold">HTML & CSS</h1>
+          <h1 className="text-2xl font-bold">HTML <span className="text-orange-500">&</span> CSS</h1>
           <p className="text-sm">design and build websites</p>
         </div>
       </div>
