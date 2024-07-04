edf77493061559eb42545e321ca4264ac1bdd3cf
Techwolf78 <mrajaypawar207@gmail.com>
2024-07-04 13:39:36 +0530

    updated logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -7,7 +7,7 @@ const Logo: React.FC = () => {
         <div className="absolute top-0 left-0 w-full h-full border-[42px] border-transparent border-t-[#ca7c4e] border-r-[#beb9b7] border-b-[#199296] border-l-[#a0e4af] transform rotate-45"></div>
         <div className="absolute inset-0 flex flex-col justify-center items-center text-white text-center">
           <h1 className="text-4xl font-bold">HTML <span className="text-orange-500">&</span> CSS</h1>
-          <p className="text-sm">design and build websites</p>
+          <p className="text-m">design and build websites</p>
         </div>
       </div>
     </div>
